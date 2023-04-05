2f246f78920d40e4fe4d1e73abfd33f92f2aaeea
kevin <dbgurum@naver.com>
2023-04-06 08:27:59 +0900

    good CI

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World, docker ci test \n')
+  response.end('Hello World, docker ci test - v2.0 \n')
 })
 
 server.listen(port)
