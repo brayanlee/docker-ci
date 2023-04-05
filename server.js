c48f6b53762c097bd8249c8fd3e0b8442322ff0c
kevin <dbgurum@naver.com>
2023-04-06 07:36:03 +0900

    good CI

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http')
+
+var port = 4000
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World, docker ci test \n')
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
+
